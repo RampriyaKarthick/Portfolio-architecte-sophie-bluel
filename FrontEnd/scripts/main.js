debae29fe77477ef1a1b8ca70ae7e0c34a537a8c
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-03-22 14:02:30 +0100

    get categories api

---
+++ FrontEnd/scripts/main.js
@@ -0,0 +1,28 @@
+let apiUrl = "http://localhost:5678/api"
+let categories = []
+function getCategories(){
+    fetch(apiUrl+"/categories")
+    .then(response => response.json())
+    .then(response => categories = response)
+    .then(() => {
+       
+        insertFilter()
+    })
+    
+}
+function insertFilter(){
+    let filterOption = document.getElementById("filter-options")
+    let tous = document.createElement("button")
+    tous.innerText ="Tous"
+    tous.id = 0
+    filterOption.appendChild(tous)
+    for(let i=0; i<categories.length ; i++){
+        // console.log(categories[i].name)
+        let filterBtn = document.createElement("button")
+        filterBtn.innerText = categories[i].name
+        filterBtn.id = categories[i].id
+        filterOption.appendChild(filterBtn)
+    }
+}
+
+getCategories()
